3b874aaf5665d0ba714d7d0fe91288fe4de26501
loveleshp <lovelesh.pandey@mabab.com>
2021-09-14 19:14:56 +0530

    assignment5

---
+++ external.js
@@ -0,0 +1,46 @@
+function add1(){
+
+	var n1= parseInt(document.getElementById('no1').value);
+	var n2= parseInt(document.getElementById('no2').value);
+
+	document.getElementById('result2').innerHTML= n1 + n2;
+
+
+}
+ function sub1(){
+
+	var n1= parseInt(document.getElementById('no1').value);
+	var n2= parseInt(document.getElementById('no2').value);
+
+	document.getElementById('result2').innerHTML= n1 - n2;
+
+
+}
+function mul1(){
+
+	var n1= parseInt(document.getElementById('no1').value);
+	var n2= parseInt(document.getElementById('no2').value);
+
+	document.getElementById('result2').innerHTML= n1 * n2;
+
+
+}
+function div1(){
+
+	var n1= parseInt(document.getElementById('no1').value);
+	var n2= parseInt(document.getElementById('no2').value);
+
+	document.getElementById('result2').innerHTML= n1 / n2;
+
+
+}
+function mod1(){
+
+	var n1= parseInt(document.getElementById('no1').value);
+	var n2= parseInt(document.getElementById('no2').value);
+
+	document.getElementById('result2').innerHTML= n1 % n2;
+
+
+}
+
